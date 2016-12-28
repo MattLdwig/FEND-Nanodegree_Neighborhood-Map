1880212ab51f882d83542fd61f6a68e8d794b359
Wapika <mattldwig@gmail.com>
2016-12-28 10:40:46 +0100

    Add marker function to viewmodel

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 var map;
+var mapSuccess = function() {
+	"use strict";
+		initMap();
+		loadFoursquarePlaces();
+}
+var mapError = function() {
+	$('#map').html("<h2>Oops. It seems there was a problem. Map couldn't be loading. </h2>")
+}
 
 function Place(data) {
-	var marker;
+
 	this.name = data.venue.name;
 	this.lat = data.venue.location.lat;
 	this.lng = data.venue.location.lng;
 	this.category = data.venue.categories[0].pluralName;
 	this.rating = data.venue.rating;
-	this.marker = {};
 }
 
 function places() {
-	var marker,
-		placeLat,
-		placeLng,
-		service;
-	var defaultLocation = 'paris';
-
 	var self = this;
 	self.listOfPlaces = ko.observableArray([]);
 	self.filteredPlaces = ko.observableArray([]);
-	self.keyword = ko.observable('');
-	//self.place = ko.observable(defaultLocation);
-
+	self.markers = ko.observableArray([]);
+	
 	self.initMap = function() {
 		map = new google.maps.Map(document.getElementById('map'), { 
     		center: {lat: 45.5016889, lng: -73.567256},
@@ -30,53 +30,19 @@ function places() {
   		});
 	};
 
-	function getLocation(location) {
-
-		var request = {
-			query: location
-		};
-
-
-		service = new google.maps.places.PlacesService(map);
-		service.textSearch(request, callback);
-	}
-
-	function callback(results, status) {
-  		if (status == google.maps.places.PlacesServiceStatus.OK) {
-    		getPlacesInLocation(results[0]);
-  		}
-  		else {
-  			console.log(status);
-  		}
-	}
-
-	function getPlacesInLocation(place) {
-		console.log(place.location);
-		placeLat = place.geometry.location.lat();
-		placeLng = place.geometry.location.lng();
-
-		map.setCenter(new google.maps.LatLng(placeLat, placeLng));
-
-		loadFoursquarePlaces();
-	}
-
-	function initializeLocation(location) {
-		getPlacesInLocation(location)
-	};
-
-
 	self.loadFoursquarePlaces = function() {
 
 	  var clientSecret = "UKDBXCJSYFG0AL3MDBMGWHHJSMAIR3S5V5NZISHJQOIKOMEP",
 	  	  clientId = "KKTBRM0OM2QIUW5TVMRYK42XV2FIU1AAN5SZY4OMOJB504VU",
+	  	  city = "montreal",
 	  	  typeOfQuery = "outdoors"
 
 	  $.getJSON("https://api.foursquare.com/v2/venues/explore"+
 	  			"?client_id="+clientId+
 	  			"&client_secret="+clientSecret+
-	  			"&query="+ self.keyword() +
+	  			"&near="+ city +
 	  			"&v=20161222"+
-	  			"&ll="+ placeLat + ',' + placeLng
+	  			"&section="+typeOfQuery
 	  			)
 	  			.done(function(data) {
 	  				var response = data.response.groups[0].items;
@@ -84,10 +50,10 @@ function places() {
 	  					 	self.listOfPlaces.push(
 	  					 		new Place(data)
 	  					 	);
-	  					 	listOfPlaces.forEach(function(place){
-	  					 		setMarkerPlace(place)
-	  					 	});
 	  					});
+	  					listOfPlaces().forEach(function(item){
+	  						placeMarker(item);
+	  					})
 	  			})
 	  			.fail(function() {
 	  				$('.error-message').html("Oops… It seems there was a problem during the request. Data couldn't be loaded from Foursquare.");
@@ -96,20 +62,58 @@ function places() {
 
 	self.userInput = ko.observable($('input').val());
 
+	self.filterPlace = ko.computed(function() {
+		self.filteredPlaces([]);
+			if(userInput() !== ""){
+				return ko.utils.arrayFilter(self.listOfPlaces(), function(item) {
+						if( item.category.toLowerCase() == userInput().toLowerCase() ) {
+							filteredPlaces.push(item);
+						} else {
+							item.marker.setMap(null);
+						}
+				});
+			} else {
+				self.filteredPlaces(listOfPlaces());
+			}
+	});
 
-	function setMarkerPlace(place) {
+
+	function placeMarker(places){
+		var marker;
+
 		marker = new google.maps.Marker ({
-	    	position: new google.maps.LatLng(place.lat, place.lng),
-	   		map: map,
-	    	title: place.name
+	    	position: new google.maps.LatLng(places.lat, places.lng),
+	    	map: map,
+	    	title: places.name,
+	    	animation: google.maps.Animation.DROP
   		});
-  		place.marker = marker;
+		places.marker = marker;
+
+		var infoWindow = new google.maps.InfoWindow({
+      		content: places.name
+    	});
+
+    	marker.infowindow = infoWindow;
+
+		google.maps.event.addListener(marker, 'click', function() {
+      		toggleBounce(marker);
+      		infoWindow.open(map, marker);
+    	});
+
 	}
-	getLocation(defaultLocation);
-	//initializeLocation(defaultLocation);
+
+	function toggleBounce(marker) {  
+  		if (marker.setAnimation() != null) {
+    		marker.setAnimation(null);
+  		} else {
+    		marker.setAnimation(google.maps.Animation.BOUNCE);
+    		setTimeout(function() {
+     		marker.setAnimation(null);
+    		}, 2000);
+  		}
+	}
+
 }
-
-
 
 ko.applyBindings(places());
 
